da04978609b3cd3cefa4a58c39f61d88966ec8d5
Michael Gerland <mr.gerland@gmail.com>
2019-10-23 15:31:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  counter = 0
+  while counter < array.length
+    new_array << yield(array[counter])
+    counter += 1
+  end
+  new_array
+end
+
+def reduce(array, starting_value=nil)
+  if starting_value
+    num = starting_value
+    counter = 0
+  else
+    num = array[0]
+    counter = 1
+  end
+  while counter < array.length
+    num = yield(num, array[counter])
+  end
+  num
+end
